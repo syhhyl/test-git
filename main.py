bf459176ed79dd19878361f335147d7b018223b9
syhhyl <syhhyl926@126.com>
2024-08-20 21:57:19 +0800

    merge b1

--- main.py
+++ main.py
@@ -2,18 +2,9 @@ for i in range(10):
     print(i)
 
 print(1)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> b1
 
 def func(a, b):
     
     print("a = ", a, "b = ", b)
     print("a + b = ", a+b)
 func(1, 3)
-<<<<<<< HEAD
->>>>>>> b1
-=======
->>>>>>> b1
